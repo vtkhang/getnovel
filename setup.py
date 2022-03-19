fa00ff593863eb72c881b33669479cac84cd5f4a
Vũ Thừa Khang <vuthuakhangit@gmail.com>
2022-03-20 03:42:25 +0900

    setuptools standardization, bug at entry_point

--- setup.py
+++ setup.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python
 """The setup and build script for novelutils."""
 
-import setuptools
+from setuptools import setup, find_packages
+import pkg_resources
 
-import novelutils
-
-with open('README.md', 'r', encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name='novelutils',
-    version=novelutils.__version__,
-    author='Eaus',
-    author_email='kdekiwis1@gmail.com',
-    license='MIT License',
-    description='Tool based on Scrapy framework to get novel from web site.',
+setup(
+    name="novelutils",
+    version=pkg_resources.require("novelutils")[0].version,
+    author="Eaus",
+    author_email="kdekiwis1@gmail.com",
+    license="MIT License",
+    description="Tool based on Scrapy framework to get novel from web site.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=setuptools.find_packages(),
+    long_description_content_type="text/markdown",
+    packages=find_packages("novelutils"),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     install_requires=[
-        'setuptools>=58.0.4',
-        'scrapy>=2.6.1',
-        'beautifulsoup4>=4.10.0',
-        'pillow>=9.0.1'
-        ],
+        "Scrapy==2.5.1",
+        "beautifulsoup4>=4.10.0",
+        "Pillow>=8.4.0",
+    ],
+    extras_require={
+        "dev": [
+            "pylint>=2.12.2",
+            "yapf>=0.32.0",
+        ]
+    },
+    python_requires=">=3.6",
     entry_points={
-        'console_scripts': ['novelutils = novelutils:run_main'],
+        "console_scripts": ["novelutils = novelutils:run_main"],
     },
 )
