d269494df062422d69372e75e716ac02e2284b51
Vũ Thừa Khang <vuthuakhangit@gmail.com>
2022-12-15 17:21:53 +0900

    define Items and ItemLoaders

--- src/getnovel/app/items.py
+++ src/getnovel/app/items.py
@@ -6,18 +6,52 @@
 """
 
 from scrapy import Field, Item
+from itemloaders.processors import MapCompose, Identity, Join
+from scrapy.loader import ItemLoader
 
 
-class NovelInfo(Item):
+class Info(Item):
     """Store novel info."""
+
     title = Field()
     author = Field()
     url = Field()
     types = Field()
     foreword = Field()
+    cover_url = Field()
+    save_path = Field()
+
+
+class InfoLoader(ItemLoader):
+    """Process info data"""
+
+    default_input_processor = MapCompose(str.strip)
+    default_output_processor = Join(" ")
+
+    def types_out(self, values):
+        return ", ".join(values)
+
+    def foreword_out(self, values):
+        return "\n".join(values)
+
+    save_path_in = Identity()
+    save_path_out = Identity()
 
 
 class Chapter(Item):
     """Store novel chapters."""
-    chapter_title = Field()
-    chapter_content = Field()
+
+    title = Field()
+    content = Field()
+
+
+class ChapterLoader(ItemLoader):
+    """Process chapter data"""
+
+    default_input_processor = MapCompose(str.strip)
+
+    def title_out(self, values):
+        return " ".join(values)
+
+    def content_out(self, values):
+        return "\n".join([x for x in values if x != ""])
