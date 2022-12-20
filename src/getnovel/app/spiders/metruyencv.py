55bea3c9f1c35d4136a1d64d05af2f2698d93af0
Vũ Thừa Khang <vuthuakhangit@gmail.com>
2022-12-20 21:56:49 +0900

    rework metruyencv

--- src/getnovel/app/spiders/metruyencv.py
+++ src/getnovel/app/spiders/metruyencv.py
@@ -1,4 +1,4 @@
-"""Get novel from domain metruyencv
+"""Get novel on domain metruyencv.
 
 .. _Web sites:
    https://metruyencv.com
@@ -6,13 +6,16 @@
 """
 from pathlib import Path
 
-import scrapy
+from scrapy import Spider, Request
+from scrapy.http import Response
+from scrapy.exceptions import CloseSpider
+
 from getnovel.app.items import Info, Chapter
 from getnovel.app.itemloaders import InfoLoader, ChapterLoader
 
 
-class MeTruyenCVSpider(scrapy.Spider):
-    """Define spider for domain: metruyencv"""
+class MeTruyenCVSpider(Spider):
+    """Declare spider for domain: metruyencv"""
 
     name = "metruyencv"
 
@@ -25,98 +28,86 @@ class MeTruyenCVSpider(scrapy.Spider):
         *args,
         **kwargs,
     ):
-        """Initialize the attributes for this spider
+        """Initialize the attributes.
 
         Parameters
         ----------
         url : str
-            The link of the novel information page
+            Url of the novel information page.
         save_path : Path
-            Path of raw directory
+            Path of raw directory.
         start_chap : int
-            Start crawling from this chapter
+            Start crawling from this chapter.
         stop_chap : int
-            Stop crawling from this chapter, input -1 to get all chapters
+            Stop crawling from this chapter, input -1 to get all chapters.
         """
         super().__init__(*args, **kwargs)
         self.start_urls = [url]
         self.save_path = save_path
         self.start_chap = start_chap
         self.stop_chap = stop_chap
-        self.total_chapter = 0
-        self.base_url = None
+        self.total = 0
 
-    def parse(self, response: scrapy.http.Response, **kwargs):
-        """Extract info of the novel and get the link of the
-        table of content (toc)
+    def parse(self, response: Response):
+        """Extract info and send request to the start chapter.
 
         Parameters
         ----------
         response : Response
-            The response to parse
+            The response to parse.
 
         Yields
         ------
         Request
-            Request to the cover image page and toc page
+            Info item.
+        Request
+            Request to the start chapter.
         """
         yield get_info(response)
-        total_chapter_str = response.xpath(
-            '//*[@id="nav-tab-chap"]/span[2]/text()'
-        ).get()
-        if total_chapter_str is None:
-            raise scrapy.exceptions.CloseSpider(
-                reason="Couldn't get total chapter number"
-            )
-        try:
-            total_chapter = int(total_chapter_str)
-        except ValueError as e:
-            raise scrapy.exceptions.CloseSpider(
-                reason="Couldn't convert total chapter number to integer"
-            ) from e
-        self.base_url = response.request.url
-        self.total_chapter = total_chapter
-        yield scrapy.Request(
-            url=self.base_url + r"/chuong-" + str(self.start_chap),
+        yield Request(
+            url=f"{response.url}/chuong-{self.start_chap}/",
             meta={"id": self.start_chap},
             callback=self.parse_content,
         )
+        self.total = int(response.xpath('//a[@id="nav-tab-chap"]/span[2]/text()').get())
 
-    def parse_content(self, response: scrapy.http.Response):
-        """Extract the content of chapter
+    def parse_content(self, response: Response):
+        """Extract content.
 
         Parameters
         ----------
         response : Response
-            The response to parse
+            The response to parse.
 
         Yields
         ------
         Request
-            Request to the next chapter
+            Chapter item.
+        Request
+            Request to the next chapter.
         """
         yield get_content(response)
-        if (
-            response.meta["id"] >= self.total_chapter
-            or response.meta["id"] == self.stop_chap
-        ):
-            raise scrapy.exceptions.CloseSpider(reason="Done")
+        next_url = (
+            f'{response.url.rsplit("/", 2)[0]}/chuong-{str(response.meta["id"] + 1)}/'
+        )
+        if response.meta["id"] == self.stop_chap or response.meta["id"] >= self.total:
+            raise CloseSpider(reason="Done")
 
-        yield scrapy.Request(
-            url=self.base_url + r"/chuong-" + str(response.meta["id"] + 1),
+        yield Request(
+            url=next_url,
             headers=response.headers,
             meta={"id": response.meta["id"] + 1},
             callback=self.parse_content,
         )
 
 
-def get_info(response: scrapy.http.Response):
-    """Get info of this novel
+def get_info(response: Response):
+    """Get novel information.
 
     Parameters
     ----------
     response : Response
-        The response to parse
+        The response to parse.
     """
     r = InfoLoader(item=Info(), response=response)
     r.add_xpath("title", '//h1[@class="h3 mr-2"]/a/text()')
@@ -128,13 +119,13 @@ def get_info(response: scrapy.http.Response):
     return r.load_item()
 
 
-def get_content(response: scrapy.http.Response):
-    """Get content of this novel
+def get_content(response: Response):
+    """Get chapter content.
 
     Parameters
     ----------
     response : Response
-        The response to parse
+        The response to parse.
     """
     r = ChapterLoader(item=Chapter(), response=response)
     r.add_xpath("title", '//div[contains(@class,"nh-read__title")]/text()')
