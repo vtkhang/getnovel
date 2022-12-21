7808246fb4e1bce48964092270d8dd7b481ff98f
Vũ Thừa Khang <vuthuakhangit@gmail.com>
2022-12-22 03:57:00 +0900

    rework

--- src/getnovel/app/spiders/sixnineshu.py
+++ src/getnovel/app/spiders/sixnineshu.py
@@ -1,37 +1,40 @@
-# -*- coding: utf-8 -*-
 """Get novels from domain 69shu.
 
 .. _Web site:
    https://www.69shu.com
 
 """
+
 from pathlib import Path
 
 from scrapy import Spider, Request
 from scrapy.http import Response
 from scrapy.exceptions import CloseSpider
 
+from getnovel.app.items import Info, Chapter
+from getnovel.app.itemloaders import InfoLoader, ChapterLoader
+
 
 class SixNineshuSpider(Spider):
-    """Define spider for domain: 69shu"""
+    """Declare spider for domain: 69shu"""
 
     name = "69shu"
 
     def __init__(
         self,
         url: str,
-        save_path: Path,
         start_chap: int,
         stop_chap: int,
+        save_path: Path,
         *args,
         **kwargs,
     ):
-        """Initialize the attributes for this spider.
+        """Initialize attributes.
 
         Parameters
         ----------
         url : str
-            The link of the novel information page.
+            Url of the novel information page.
         save_path : Path
             Path of raw directory.
         start_chap : int
@@ -41,14 +44,13 @@ class SixNineshuSpider(Spider):
         """
         super().__init__(*args, **kwargs)
         self.start_urls = [url]
-        self.sp = save_path
-        self.sa = start_chap
-        self.so = stop_chap
-        self.toc: list = []
+        self.start_chap = start_chap
+        self.stop_chap = stop_chap
+        self.save_path = save_path
+        self.total_chap = 0
 
-    def parse(self, response: Response, **kwargs):
-        """Extract info of the novel and get the link of the
-        table of content (toc).
+    def parse(self, response: Response):
+        """Extract info and send request to the start chapter.
 
         Parameters
         ----------
@@ -58,22 +60,16 @@ class SixNineshuSpider(Spider):
         Yields
         ------
         Request
-            Request to the cover image page and toc page.
+            Info item.
+        Request
+            Request to the start chapter.
         """
-        # download cover
-        c_url = response.xpath('//*[@class="bookimg2"]/img/@src').get()
-        yield Request(
-            url=c_url,
-            callback=self.parse_cover,
-        )
-        get_info(response, self.sp)
-        toc_str = response.xpath('//*[@class="btn more-btn"]/@href').get()
-        toc_url = f"https://www.69shu.com{toc_str}"
-        yield Request(url=toc_url, callback=self.parse_toc)
+        yield get_info(response)
+        toc_url = response.xpath("//div[3]//div[3]/a/@href").get()
+        yield Request(url=response.urljoin(toc_url), callback=self.parse_start)
 
-    def parse_toc(self, response: Response) -> None:
-        """Extract info of the novel and get the link of the
-        table of content (toc).
+    def parse_start(self, response: Response):
+        """Extract link of the start chapter.
 
         Parameters
         ----------
@@ -85,29 +81,20 @@ class SixNineshuSpider(Spider):
         Request
             Request to the start chapter.
         """
-        self.toc: list = response.xpath('//*[@id="catalog"]/ul/li/a/@href').getall()
-        if self.sa > len(self.toc):
+        chapter_links = response.xpath('//*[@id="catalog"]//a/@href').getall()
+        self.total_chap = len(chapter_links)
+        if self.start_chap > self.total_chap:
             raise CloseSpider(
                 reason="Start chapter index is greater than total chapters"
             )
         yield Request(
-            url=self.toc[self.sa - 1],  # goto start chapter
-            meta={"id": self.sa},
+            url=chapter_links[self.start_chap - 1],
+            meta={"id": self.start_chap},
             callback=self.parse_content,
         )
 
-    def parse_cover(self, response: Response):
-        """Download the cover of novel.
-
-        Parameters
-        ----------
-        response : Response
-            The response to parse.
-        """
-        (self.sp / "cover.jpg").write_bytes(response.body)
-
     def parse_content(self, response: Response):
-        """Extract the content of chapter.
+        """Extract content.
 
         Parameters
         ----------
@@ -117,64 +104,53 @@ class SixNineshuSpider(Spider):
         Yields
         ------
         Request
+            Chapter item.
+        Request
             Request to the next chapter.
         """
-        get_content(response, self.sp)
-        if (response.meta["id"] == len(self.toc)) or (response.meta["id"] == self.so):
+        yield get_content(response)
+        next_url = response.xpath("//div[3]//a[4]/@href").get()
+        if (response.meta["id"] == self.total_chap) or (
+            response.meta["id"] == self.stop_chap
+        ):
             raise CloseSpider(reason="Done")
-        link_next_chap = self.toc[response.meta["id"]]
-        response.request.headers[b"Referer"] = [str.encode(response.url)]
         yield Request(
-            url=link_next_chap,
-            headers=response.request.headers,
+            url=response.urljoin(next_url),
             meta={"id": response.meta["id"] + 1},
             callback=self.parse_content,
         )
 
 
-def get_info(response: Response, save_path: Path) -> None:
-    """Get info of this novel.
+def get_info(response: Response):
+    """Get novel information.
 
     Parameters
     ----------
     response : Response
         The response to parse.
-    save_path : Path
-        Path of raw directory.
     """
-    # extract info
-    title: str = response.xpath('//*[@class="booknav2"]/h1/a/text()').get()
-    t = response.xpath('//*[@class="booknav2"]/p/a/text()').getall()
-    author: str = t[0]
-    types = ["--"]
-    if len(t) == 2:
-        types = t[1]
-    foreword = response.xpath('//*[@class="navtxt"]/p[1]/text()').getall()
-    info = []
-    info.append(title)
-    info.append(author)
-    info.append(response.request.url)
-    info.append(", ".join(types))
-    info.extend(foreword)
-    # write info to file
-    (save_path / "foreword.txt").write_text("\n".join(info), encoding="utf-8")
+    r = InfoLoader(item=Info(), response=response)
+    r.add_xpath("title", '//*[@class="booknav2"]/h1//text()')
+    r.add_xpath("author", '//*[@class="booknav2"]/p/a/text()')
+    r.add_value("types", "--")
+    r.add_xpath("foreword", '//*[@class="navtxt"]/p[1]/text()')
+    r.add_xpath("image_urls", '//*[@class="bookimg2"]/img/@src')
+    r.add_value("url", response.request.url)
+    return r.load_item()
 
 
-def get_content(response: Response, save_path: Path) -> None:
-    """Get content of this novel.
+def get_content(response: Response):
+    """Get chapter content.
 
     Parameters
     ----------
     response : Response
         The response to parse.
-    save_path : Path
-        Path of raw directory.
     """
-    content: list = response.xpath(
-        '//*[@class="txtnav"]//text()[not(parent::h1) '
-        "and not(parent::span) and not(parent::script)]"
-    ).getall()
-    # content.insert(0, chapter)
-    (save_path / f'{str(response.meta["id"])}.txt').write_text(
-        "\n".join([x.strip() for x in content if x.strip() != ""]), encoding="utf-8"
-    )
+    r = ChapterLoader(item=Chapter(), response=response)
+    r.add_xpath("title", "//div[3]//h1/text()")
+    content = response.xpath('//div[@class="txtnav"]/text()').getall()
+    content[3] = ''
+    r.add_value("content", content)
+    r.add_value("id", str(response.meta["id"]))
+    return r.load_item()
