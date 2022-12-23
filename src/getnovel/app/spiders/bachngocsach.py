552a2a21094e0f1f371f8beb243745044fb85a57
Vũ Thừa Khang <vuthuakhangit@gmail.com>
2022-12-23 18:28:43 +0900

    clarify docstring

--- src/getnovel/app/spiders/bachngocsach.py
+++ src/getnovel/app/spiders/bachngocsach.py
@@ -1,4 +1,4 @@
-"""Get novel from domain bachngocsach.
+"""Get novel on domain bachngocsach.
 
 .. _Web site:
    https://bachngocsach.com.vn/reader
@@ -7,7 +7,7 @@
 
 from pathlib import Path
 
-from scrapy import Spider, Request
+from scrapy import Spider
 from scrapy.http import Response
 from scrapy.exceptions import CloseSpider
 
@@ -16,7 +16,7 @@ from getnovel.app.itemloaders import InfoLoader, ChapterLoader
 
 
 class BachNgocSachSpider(Spider):
-    """Declare spider for domain: bachngocsach"""
+    """Define spider for domain: bachngocsach"""
 
     name = "bachngocsach"
 
@@ -40,7 +40,7 @@ class BachNgocSachSpider(Spider):
         start_chap : int
             Start crawling from this chapter.
         stop_chap : int
-            Stop crawling from this chapter, input -1 to get all chapters.
+            Stop crawling at this chapter, input -1 to get all chapters.
         """
         super().__init__(*args, **kwargs)
         self.start_urls = [url]
@@ -49,7 +49,7 @@ class BachNgocSachSpider(Spider):
         self.save_path = save_path
 
     def parse(self, response: Response):
-        """Extract info and send request to the start chapter.
+        """Extract info and send request to table of content.
 
         Parameters
         ----------
@@ -58,13 +58,15 @@ class BachNgocSachSpider(Spider):
 
         Yields
         ------
-        Request
+        Info
             Info item.
         Request
-            Request to the start chapter.
+            Request to table of content.
         """
         yield get_info(response)
-        yield Request(url=f"{response.url}/muc-luc?page=all", callback=self.parse_start)
+        yield response.follow(
+            url=f"{response.url}/muc-luc?page=all", callback=self.parse_start
+        )
 
     def parse_start(self, response: Response):
         """Extract link of the start chapter.
@@ -79,11 +81,10 @@ class BachNgocSachSpider(Spider):
         Request
             Request to the start chapter.
         """
-        start_url = response.xpath('//*[@class="chuong-link"]/@href').getall()[
-            self.start_chap - 1
-        ]
-        yield Request(
-            url=f"https://bachngocsach.com.vn{start_url}",
+        yield response.follow(
+            url=response.xpath(
+                f'(//*[@class="chuong-link"])[{self.start_chap}]'
+            ).attrib["href"],
             meta={"id": self.start_chap},
             callback=self.parse_content,
         )
@@ -98,17 +99,17 @@ class BachNgocSachSpider(Spider):
 
         Yields
         ------
-        Request
+        Chapter
             Chapter item.
         Request
             Request to the next chapter.
         """
         yield get_content(response)
-        next_url = response.xpath('//a[contains(@class,"page-next")]/@href').get()
-        if not next_url or response.meta["id"] == self.stop_chap:
+        next_url = response.xpath('//a[contains(@class,"page-next")]').attrib["href"]
+        if (not next_url) or (response.meta["id"] == self.stop_chap):
             raise CloseSpider(reason="Done")
-        yield Request(
-            url=f"https://bachngocsach.com.vn{next_url}",
+        yield response.follow(
+            url=next_url,
             meta={"id": response.meta["id"] + 1},
             callback=self.parse_content,
         )
@@ -121,6 +122,11 @@ def get_info(response: Response):
     ----------
     response : Response
         The response to parse.
+
+    Returns
+    -------
+    Info
+        Populated Info item.
     """
     r = InfoLoader(item=Info(), response=response)
     r.add_xpath("title", '//*[@id="truyen-title"]/text()')
@@ -139,6 +145,11 @@ def get_content(response: Response):
     ----------
     response : Response
         The response to parse.
+
+    Returns
+    -------
+    Chapter
+        Populated Chapter item.
     """
     r = ChapterLoader(item=Chapter(), response=response)
     r.add_xpath("title", '//h1[@id="chuong-title"]/text()')
