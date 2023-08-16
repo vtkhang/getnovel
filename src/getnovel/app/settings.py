06baf72b21e1a4bbb6286f73d50b8ba4287f4109
vtkhang <vuthuakhangit@gmail.com>
2023-08-16 21:23:07 +0700

    reformat

--- src/getnovel/app/settings.py
+++ src/getnovel/app/settings.py
@@ -1,8 +1,10 @@
 """These settings only use for scrapy shell."""
 
-BOT_NAME = r"GetNovel"
+BOT_NAME = "GetNovel"
 ROBOTSTXT_OBEY = True
 SPIDER_MODULES = ["getnovel.app.spiders"]
-USER_AGENT = r"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " \
-             "(KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"
+)
 COOKIES_DEBUG = True
