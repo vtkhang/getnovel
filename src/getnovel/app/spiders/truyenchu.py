e29bd3453760bb20379e22095f7474424336f4b7
vtkhang <vuthuakhangit@gmail.com>
2023-09-02 23:16:00 +0700

    rework truyenchu

--- src/getnovel/app/spiders/truyenchu.py
+++ src/getnovel/app/spiders/truyenchu.py
@@ -1,6 +1,6 @@
 """Get novel on domain truyenchu.
 
-.. _Web site:
+.. _Website:
    https://truyenchu.vn/
 
 """
@@ -16,41 +16,38 @@ from getnovel.app.items import Chapter, Info
 class TruyenChuSpider(Spider):
     """Define spider for domain: truyenchu.
 
-    Attributes:
+    Attributes
     ----------
     name : str
         Name of the spider.
-    start_urls : list
-        List of url to start crawling from.
-    sa : int
-        The chapter index to start crawling.
-    so : int
-        The chapter index to stop crawling after that.
-    c : str
+    title_pos : int
+        Position of the title in the novel url.
+    lang : str
         Language code of novel.
     """
 
     name = "truyenchu"
+    title_pos = -1
+    lang_code = "vi"
 
-    def __init__(self, u: str, start: int, stop: int, *args, **kwargs):
+    def __init__(self: "TruyenChuSpider", url: str, start: int, stop: int) -> None:
         """Initialize attributes.
 
         Parameters
         ----------
-        u : str
+        url : str
             Url of the novel information page.
         start: int
             Start crawling from this chapter.
         stop : int
             Stop crawling after this chapter, input -1 to get all chapters.
         """
-        super().__init__(*args, **kwargs)
-        self.start_urls = [u]
+        self.start_urls = [url]
         self.sa = int(start)
         self.so = int(stop)
         self.c = "vi"  # language code
 
-    def parse(self, res: Response):
+    def parse(self: "TruyenChuSpider", res: Response) -> None:
         """Extract info and send request to the table of content.
 
         Parameters
@@ -58,7 +55,7 @@ class TruyenChuSpider(Spider):
         res : Response
             The response to parse.
 
-        Yields:
+        Yields
         ------
         Info
             Info item.
@@ -83,7 +80,7 @@ class TruyenChuSpider(Spider):
             },
         )
 
-    def parse_toc(self, res: Response):
+    def parse_toc(self: "TruyenChuSpider", res: Response) -> None:
         """Extract link of the start chapter.
 
         Parameters
@@ -91,7 +88,7 @@ class TruyenChuSpider(Spider):
         res : Response
             The response to parse.
 
-        Yields:
+        Yields
         ------
         Request
             Request to the start chapter.
@@ -105,7 +102,7 @@ class TruyenChuSpider(Spider):
             callback=self.parse_content,
         )
 
-    def parse_content(self, res: Response):
+    def parse_content(self: "TruyenChuSpider", res: Response) -> None:
         """Extract content.
 
         Parameters
@@ -113,7 +110,7 @@ class TruyenChuSpider(Spider):
         res : Response
             The response to parse.
 
-        Yields:
+        Yields
         ------
         Chapter
             Chapter item.
@@ -140,7 +137,7 @@ def get_info(res: Response) -> Info:
     res : Response
         The response to parse.
 
-    Returns:
+    Returns
     -------
     Info
         Populated Info item.
@@ -164,7 +161,7 @@ def get_content(res: Response) -> Chapter:
     res : Response
         The response to parse.
 
-    Returns:
+    Returns
     -------
     Chapter
         Populated Chapter item.
